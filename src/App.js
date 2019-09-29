46452fc6770576b88d69d80c6e1501abad6d489a
xiangxu <iasonliu@gmail.com>
2019-09-29 17:31:44 +0800

    add style and using es6 functions

--- src/App.js
+++ src/App.js
@@ -31,7 +31,6 @@ class App extends Component {
     };
 
     this.onDismiss = this.onDismiss.bind(this);
-    this.onClickMe = this.onClickMe.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
   }
   onDismiss(id) {
@@ -39,42 +38,66 @@ class App extends Component {
     const updateList = this.state.list.filter(isNotId);
     this.setState({ list: updateList });
   }
-  onClickMe() {
-    console.log(this);
-  }
+
   onSearchChange(event) {
     this.setState({ searchTerm: event.target.value });
   }
   render() {
     const { searchTerm, list } = this.state;
     return (
-      <div className="App">
-        <form>
-          <input type="text" value={searchTerm} onChange={this.onSearchChange} />
-        </form>
-        {list.filter(isSearched(searchTerm)).map((item) => (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <button onClick={() => this.onDismiss(item.objectID)} type="button">
-                Dismiss
-              </button>
-            </span>
-            <span>
-              <button onClick={this.onClickMe} type="button">
-                Click Me
-              </button>
-            </span>
-          </div>
-        ))}
+      <div className="page">
+        <div className="interactions">
+          <Search value={searchTerm} onChange={this.onSearchChange}>
+            Search
+          </Search>
+        </div>
+
+        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
       </div>
     );
   }
 }
+
+const Search = ({ value, onChange, children }) => {
+  return (
+    <form>
+      {children}
+      <input type="text" value={value} onChange={onChange} />
+    </form>
+  );
+};
+
+const Table = ({ list, pattern, onDismiss }) => {
+  const largeColumn = { width: '40%' };
+  const midColumn = { width: '30%' };
+  const smallColumn = { width: '10%' };
+  return (
+    <div className="table">
+      {list.filter(isSearched(pattern)).map((item) => (
+        <div key={item.objectID} className="table-row">
+          <span style={largeColumn}>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span style={midColumn}>{item.author}</span>
+          <span style={smallColumn}>{item.num_comments}</span>
+          <span style={smallColumn}>{item.points}</span>
+          <span style={smallColumn}>
+            <Button className="button-inline" onClick={() => onDismiss(item.objectID)}>
+              Dismiss
+            </Button>
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const Button = ({ onClick, className = '', children }) => {
+  return (
+    <button onClick={onClick} className={className} type="button">
+      {children}
+    </button>
+  );
+};
 
 export default App;
