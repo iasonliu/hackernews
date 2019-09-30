c817d4d1a3182c696ba96d76d5beb2455b5eb017
xiangxu <iasonliu@gmail.com>
2019-09-30 22:58:29 +0800

    add jestjs testing

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import App from '.';
-describe('App', () => {
+import Button from '.';
+
+describe('Button', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(<Button>Give Me More</Button>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
   test('has a valid snapshot', () => {
-    const component = renderer.create(<App />);
+    const component = renderer.create(<Button>Give Me More</Button>);
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
--- src/components/Search/Search.test.js
+++ src/components/Search/Search.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import App from '.';
-describe('App', () => {
+import Search from '.';
+describe('Search', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(<Search>Search</Search>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
   test('has a valid snapshot', () => {
-    const component = renderer.create(<App />);
+    const component = renderer.create(<Search>Search</Search>);
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
